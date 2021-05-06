ac789d30cb9f81b346a85a353d99000cf5b09e46
afelipevargas <devafvargas@gmail.com>
2021-05-06 23:54:47 +0000

    TASK0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,10 +9,6 @@ size_t print_dlistint(const dlistint_t *h)
     const dlistint_t *aux = NULL;
     size_t number_nodes = 0;
 
-    if (h == NULL)
-    {
-        return (0);
-    }
     aux = h;
     while (aux)
     {
