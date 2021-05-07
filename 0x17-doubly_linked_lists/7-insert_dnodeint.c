8862efe0ab8cccd38f998a7d170fc3bf74f61824
afelipevargas <devafvargas@gmail.com>
2021-05-07 08:53:19 -0500

    TASK7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,35 +1,36 @@
 #include "lists.h"
 /**
- * sum_dlistint - returns the sum of all the data (n)
- *                    of a dlistint_t linked list.
- * @head: pointer to firts node.
- * Return: the sum of all the data (n) of a dlistint_t linked list.
+ * insert_dnodeint_at_index - inserts a new node at a given position.
+ * @h: pointer to firts node.
+ * @idx: number of node to add.
+ * @n: number.
+ * Return:  the address of the new node, or NULL if it failed.
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *aux = NULL;
-    dlistint_t *node = NULL;
-    unsigned int index = 0;
+dlistint_t *aux = NULL;
+dlistint_t *node = NULL;
+unsigned int index = 0;
 
-    node = malloc(sizeof(dlistint_t));
-    if (node == NULL)
-    {
-        return (NULL);
-    }
-    aux = *h;
-    node->n = n;
-    while (index < idx)
-    {
-        aux = aux->next;
-        index++;
-    }
-    if (aux->next == NULL)
-    {
-        return (add_dnodeint_end(h, n));
-    }
-    node->next = aux;
-    node->prev = aux->prev;
-    aux->prev->next = node;
-    aux->prev = node;
-    return (node);
+node = malloc(sizeof(dlistint_t));
+if (node == NULL)
+{
+return (NULL);
+}
+aux = *h;
+node->n = n;
+while (index < idx)
+{
+aux = aux->next;
+index++;
+}
+if (aux->next == NULL)
+{
+return (add_dnodeint_end(h, n));
+}
+node->next = aux;
+node->prev = aux->prev;
+aux->prev->next = node;
+aux->prev = node;
+return (node);
 }
